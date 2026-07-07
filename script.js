5b534b36e6b40b973408f9ebae907844c131122d
gnyana3chaitanya <39720146+gnyana3chaitanya@users.noreply.github.com>
2026-07-07 11:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const button = document.getElementById("start-btn");
 
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+
+    const html5QrCode = new Html5Qrcode("reader");
+
+    html5QrCode.start(
+
+        {
+            facingMode: "environment"
+        },
+
+        {
+            fps: 10,
+            qrbox: 250
+        },
+
+        (decodedText) => {
+
+            result.innerHTML =
+
+            `
+            <h2>✅ QR Detected</h2>
+
+            <p>${decodedText}</p>
+            `;
+
+            html5QrCode.stop();
+
+        },
+
+        (errorMessage) => {
+
+            // Ignore scan errors
+
+        }
+
+    );
+
+});
