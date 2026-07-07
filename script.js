b31bdecead5642681683a6930dbeb4aa62b405a4
gnyana3chaitanya <39720146+gnyana3chaitanya@users.noreply.github.com>
2026-07-07 18:29:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const LOGIN_WEBHOOK = "https://chaitu1.app.n8n.cloud/webhook-test/organizer-login";
+const loginButton = document.getElementById("login-btn");
+
+const usernameInput = document.getElementById("username");
+
+const passwordInput = document.getElementById("password");
+loginButton.addEventListener("click", async () => {
+
+    const username = usernameInput.value;
+
+    const password = passwordInput.value;
+
+    console.log(username);
+
+    console.log(password);
+
+});
 const button = document.getElementById("start-btn");
 
 const result = document.getElementById("result");
