2bacd19c540c09bdd36957796c84a334e9df019f
gnyana3chaitanya <39720146+gnyana3chaitanya@users.noreply.github.com>
2026-07-13 23:15:44 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,34 +1,95 @@
+// ======================================
+// GITAM Event Management System
+// Dashboard.js Version 1.0
+// ======================================
+
 document.addEventListener("DOMContentLoaded", loadDashboard);
 
 async function loadDashboard() {
 
-    const organizer = JSON.parse(sessionStorage.getItem("organizer"));
+    // Get organizer from sessionStorage
 
-    if (!organizer) {
-        window.location.href = "index.html";
+    const organizer = JSON.parse(
+        sessionStorage.getItem("organizer")
+    );
+
+    if (!organizer){
+
+        window.location.href="index.html";
+
         return;
+
     }
 
-    try {
+    // Display organizer details
 
-        const response = await fetch(DASHBOARD_API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                organizer_id: organizer.organizer_id
-            })
-        });
+    document.getElementById("organizerName").innerText =
+        organizer.organizer_name;
+
+    document.getElementById("organizerRole").innerText =
+        organizer.role;
+
+    document.getElementById("welcomeName").innerText =
+        organizer.organizer_name;
+
+    console.log("Logged in Organizer");
+
+    console.log(organizer);
+
+    try{
+
+        const response = await fetch(
+
+            DASHBOARD_API,
+
+            {
+
+                method:"POST",
+
+                headers:{
+
+                    "Content-Type":"application/json"
+
+                },
+
+                body:JSON.stringify({
+
+                    organizer_id:organizer.organizer_id
+
+                })
+
+            }
+
+        );
 
         const result = await response.json();
 
+        console.log("Dashboard API Response");
+
         console.log(result);
 
-    } catch (err) {
+    }
 
-        console.error(err);
+    catch(error){
+
+        console.error(error);
 
     }
 
 }
+
+// Logout
+
+document.getElementById("logoutBtn").addEventListener(
+
+    "click",
+
+    ()=>{
+
+        sessionStorage.clear();
+
+        window.location.href="index.html";
+
+    }
+
+);
