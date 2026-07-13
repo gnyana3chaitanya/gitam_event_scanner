65c418b34724e5cf2bc811efde63e34c2a1799bb
gnyana3chaitanya <39720146+gnyana3chaitanya@users.noreply.github.com>
2026-07-13 23:00:56 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,11 @@
+// ===============================
+// API ENDPOINTS
+// ===============================
 
+// Login API
+const LOGIN_API =
+"https://chaitu1.app.n8n.cloud/webhook-test/dashboard";
+
+// Dashboard API
+const DASHBOARD_API =
+"https://chaitu1.app.n8n.cloud/webhook-test/dashboard";
