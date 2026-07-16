e9327048af286f2710bc362443a935aab6babf62
gnyana3chaitanya <39720146+gnyana3chaitanya@users.noreply.github.com>
2026-07-16 05:20:11 +0000

    Update api.js

--- api.js
+++ api.js
@@ -4,12 +4,12 @@
 
 // Login API
 const LOGIN_API =
-"https://chaitu1.app.n8n.cloud/webhook-test/organizer-login";
+"https://gnyana.app.n8n.cloud/webhook-test/organizer-login";
 
 // Dashboard API
 const DASHBOARD_API =
-"https://chaitu1.app.n8n.cloud/webhook-test/dashboard";
+"https://gnyana.app.n8n.cloud/webhook-test/dashboard";
 
 // Dashboard Statistics API
 const DASHBOARD_STATS_API =
-"https://chaitu1.app.n8n.cloud/webhook-test/statistics";
+"https://gnyana.app.n8n.cloud/webhook-test/statistics";
